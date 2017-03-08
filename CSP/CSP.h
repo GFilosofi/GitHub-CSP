0ab04e2fd414aa05a532c6a3725787c5cb7d3d49
GFilosofi <g.filosofi@me.com>
2017-03-08 11:55:19 +0100

    Initial Commit

---
+++ CSP/CSP.h
@@ -0,0 +1,13 @@
+//
+//  CSP.h
+//  CSP
+//
+//  Created by Gabriele Filosofi on 08/03/2017.
+//  Copyright © 2017 Gabriele Filosofi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSP : NSObject
+
+@end
---
+++ CSP/CSP.m
@@ -0,0 +1,13 @@
+//
+//  CSP.m
+//  CSP
+//
+//  Created by Gabriele Filosofi on 08/03/2017.
+//  Copyright © 2017 Gabriele Filosofi. All rights reserved.
+//
+
+#import "CSP.h"
+
+@implementation CSP
+
+@end
